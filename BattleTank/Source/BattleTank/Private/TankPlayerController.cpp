d6a9ccdb12eafd81a2747cfb03f26bc71d14e9f4
jeltethefacer <j.w.r.boelens@gmail.com>
2016-08-23 15:23:24 +0200

    S4L20 using virtual and override.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,20 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if(!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Not possesing a tank"));
+	}
+	else{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is Possesing: %s"), *ControlledTank->GetName());
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
